721c98cb0375e86953d0b40a51a61b33bf27a3df
Henrique Fernandez <rickfernandez03@gmail.com>
2024-01-24 18:05:43 -0300

    feat: alter table user drop address columns

---
+++ meteorastore-backend/src/main/resources/db/migration/V3__alter-table-user-drop-address-coluns.sql
@@ -0,0 +1,9 @@
+ALTER TABLE users
+DROP COLUMN street,
+DROP COLUMN number,
+DROP COLUMN complement,
+DROP COLUMN neighborhood,
+DROP COLUMN zipCode,
+DROP COLUMN city,
+DROP COLUMN state,
+DROP COLUMN country;
