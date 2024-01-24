dc90b92a1b627592e94ea73a7003d5a3495000d6
Henrique Fernandez <rickfernandez03@gmail.com>
2024-01-24 18:03:20 -0300

    build: initial configs

---
+++ meteorastore-backend/src/main/java/com/meteorastorebackend/MeteorastoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.meteorastorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteorastoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteorastoreBackendApplication.class, args);
+	}
+
+}
---
+++ meteorastore-backend/src/test/java/com/meteorastorebackend/MeteorastoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteorastorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteorastoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
